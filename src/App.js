a95f2f3eea40dba90516ff427d41bf6dbf475f37
jordamon <jordandhunt01@gmail.com>
2023-06-26 11:57:49 +0100

    Project complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import Products from './components/pages/Products';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/sign-up' element={<SignUp />} />
       </Routes>
     </Router>
   );
